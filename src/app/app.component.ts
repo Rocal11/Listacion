b54a5b5b0fc2757d9af33c693f4c12bcbbae38c4
Rocal11 <luisrocal@gmail.com>
2023-11-29 20:02:05 -0600

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Listacion';
+  todos: Array<{done: boolean; text: string}> = [];
+
+  add(text: string) {
+    this.todos.push({text, done: false});
+  }
+
+  toggle(index: number) {
+    this.todos[index].done = !this.todos[index].done;
+  }
 }
+  //todos: string[] = [];
+
+
